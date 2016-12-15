e039ce290ff097b5e97c62d41106b1045287f2f0
syntonym <syntonym@mensa.uberspace.de>
2016-12-15 11:22:49 +0100

    fixed

--- make_doc.py
+++ make_doc.py
@@ -2,41 +2,80 @@ from btrfs_sxbackup.cli import parser
 import argparse
 import os.path
 
+import jinja2
+
 def get_subparsers(parser):
     for action in parser._actions:
         if isinstance(action, argparse._SubParsersAction):
             yield action
 
 template = """
-{prog_name}-{command}
-{prog_name_length}={command_length}
+:orphan:
+
+{{ prog_name.replace("_", "-") }}{% if command %}-{{command}}{% endif %}
+{{ "="*len(prog_name)}}={{"="*len(command)}}
 
 Synopsis
 --------
 
 .. autoprogram:: btrfs_sxbackup.cli:parser
     :maxdepth: 1
-    :start_command: {command}
-    :prog: {prog_name}
+    :prog: {{ prog_name }}
+    {% if command -%}
+    :start_command: {{ command }}
+    {%- endif %}
 
+{% if description -%}
+Description
+-----------
+
+{{ description }}
+{%- endif -%}
+
+{%- if see_also -%}
 See also
 --------
 
-:manpage:`{prog_name}(1)`
+{% for page in see_also -%}
+:manpage:`{{ page.replace("_", "-") }}(1)`
+{% endfor %}
+{%- endif -%}
 """
 
-def make_rts(prog_name, command, directory):
-    with open(os.path.join(directory, command+".rst"), mode="w") as f:
-        f.write(template.format(command=command,
-            command_length=len(command)*"=",
-            prog_name=prog_name.replace("_", "-"),
-            prog_name_length=len(prog_name)*"="))
-    
+main_template = """
+Manpages
+========
+
+.. toctree:: 
+
+    {% for page in pages -%}
+    {{ page }}
+    {% endfor %}
+"""
+
+def make_rts(name, prog_name, command, description, see_also, directory, j2_template):
+    with open(os.path.join(directory, name+".rst"), mode="w") as f:
+        f.write(j2_template.render(prog_name=prog_name,
+            command=command,
+            description=description,
+            see_also=see_also,
+            len=len))
 
 def make_rsts(parser, prog_name):
+    subpages = []
+    j2_template = jinja2.Template(template)
     for subparser in get_subparsers(parser):
         for choice in subparser.choices:
-            make_rts(str(prog_name), choice, "./docs/man_pages")
+            subpages.append(choice)
+            make_rts(choice, str(prog_name), choice, "", [str(prog_name)], "./docs/man_pages", j2_template)
+
+    see_also = [str(prog_name).replace("_", "-") + "-" + page for page in subpages]
+    make_rts(str(prog_name), str(prog_name), "", "", see_also, "./docs/man_pages", j2_template)
+
+    pages = ["man_pages/" + page for page in subpages] + ["man_pages/" + str(prog_name)]
+
+    with open(os.path.join("./docs/", "manpages.rst"), mode="w") as f:
+        f.write(jinja2.Template(main_template).render(pages=pages))
 
 
 import btrfs_sxbackup.cli
